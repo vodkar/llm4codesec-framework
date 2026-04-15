bf404e7bbe93bc2cf5d9688660213a12218f9bf1
vodkar <boombarah@gmail.com>
2026-04-15 22:55:31 +0500

    Updated build script

--- scripts/run_build_datasets.sh
+++ scripts/run_build_datasets.sh
@@ -11,4 +11,9 @@ $run_benchmark src/entrypoints/loaders/run_setup_context_assembler_compare_ranki
 $run_benchmark src/entrypoints/loaders/run_setup_context_assembler_datasets.py
 $run_benchmark src/entrypoints/loaders/run_setup_jitvul_dataset.py --data-file benchmarks/JitVul/data/final_benchmark.jsonl
 $run_benchmark src/entrypoints/setup_cleanvul_dataset.py
+
+mkdir -p benchmarks/PrimeVul
+wget --no-check-certificate \
+     "https://drive.usercontent.google.com/download?id=12b1QkCwW0SC6l9KvxSmMe4jHF7VhjwCa&confirm=t" \
+     -O "benchmarks/PrimeVul/primevul.jsonl"
 $run_benchmark src/entrypoints/setup_primevul_dataset.py
